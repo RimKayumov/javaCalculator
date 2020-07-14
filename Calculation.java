8db0b058ef0e248682aa001441732f3a16bffc8d
RimKayumov <53238409+RimKayumov@users.noreply.github.com>
2020-07-15 01:02:30 +0500

    Add files via upload
    second commit

--- Calculation.java
+++ Calculation.java
@@ -15,7 +15,7 @@ public class Calculation {
     public Calculation(String x, String y, char c, ValueType val_type) {
         switch (val_type){
             case Roma -> {
-
+                //if (y.equals(0)) throw new RuntimeException(new Error("Values can't to be less than 1"));
                 if (romaValues.get().contains(x) && romaValues.get().contains(y)) {
                     for (int i = 0; i < 10; ++i) {
                         if (romaValues.get().elementAt(i).equals(x)) {
@@ -36,9 +36,9 @@ public class Calculation {
                 firstVal = Integer.parseInt(x);
                 secondVal = Integer.parseInt(y);
                 if (firstVal > 10) throw new RuntimeException(new Error("First value is more than 10"));
-                else if (firstVal < 0) throw new RuntimeException(new Error("First value is less than 0"));
+                else if (firstVal < 1) throw new RuntimeException(new Error("First value is less than 1"));
                 if (secondVal > 10) throw new RuntimeException(new Error("Second value is more than 10"));
-                else if (secondVal < 0) throw new RuntimeException(new Error("Second value is less than 0"));
+                else if (secondVal < 1) throw new RuntimeException(new Error("Second value is less than 1"));
                 operation = new Operation(firstVal, secondVal, c);
                 resultInt = operation.getResult();
                 break;
